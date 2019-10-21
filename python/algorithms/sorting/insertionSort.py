63c272d7e19f5acb9a7144c2b2118b835a6a2657
Shanteshwar <inde.shan@gmail.com>
2019-10-21 23:30:45 +0530

    adding insertion sort

---
+++ python/algorithms/sorting/insertionSort.py
@@ -0,0 +1,16 @@
+def insertion_sort(collection):
+	for loop_index in range(1, len(collection)):
+		insertion_index = loop_index
+		while (insertion_index > 0 and collection[insertion_index - 1] > collection[insertion_index]):
+			collection[insertion_index], collection[insertion_index - 1] = (
+				collection[insertion_index - 1],
+				collection[insertion_index],
+			)
+			insertion_index -= 1
+	return collection
+
+
+if __name__ == "__main__":
+	user_input = input("Enter numbers separated by a comma:\n").strip()
+	unsorted = [int(item) for item in user_input.split(",")]
+	print(insertion_sort(unsorted))
