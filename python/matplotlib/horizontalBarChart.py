0a8a06ea9d80b42eae4a59174c5941f53eecc793
Shanteshwar <inde.shan@gmail.com>
2019-12-02 22:35:43 +0530

    adding horizontal bar chart

---
+++ python/matplotlib/horizontalBarChart.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcdefaults()
+fig, ax = plt.subplots()
+
+people = ('Shan', 'Nick', 'Sam', 'Anky', 'Jimmy')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+ax.barh(y_pos, performance, xerr=error, align='center',
+        color='green', ecolor='black')
+ax.set_yticks(y_pos)
+ax.set_yticklabels(people)
+ax.invert_yaxis()
+ax.set_xlabel('Performance')
+ax.set_title('How fast do you want to go today?')
+plt.show()
